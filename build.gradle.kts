1a63dee6fe5126f609102a9521a3a95505b39bb6
baristomruk-max <baris.tomruk@gmail.com>
2026-09-16 01:28:49 +0300

    Major update: sync build.gradle.kts with reference project (AGP 9.1.1, Kotlin 2.4.0, Gradle 9.6.1, Java 17)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
+import com.android.build.api.dsl.LibraryExtension
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-import com.android.build.gradle.BaseExtension
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 buildscript {
     repositories {
@@ -9,9 +11,9 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("com.android.tools.build:gradle:9.1.1")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0")
     }
 }
 
@@ -23,65 +25,76 @@ allprojects {
     }
 }
 
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
+    extensions.getByType<CloudstreamExtension>().configuration()
 
-fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
+fun Project.android(configuration: LibraryExtension.() -> Unit) =
+    extensions.getByType<LibraryExtension>().configuration()
 
 subprojects {
     apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/BTcozum/BTVault")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/baristomruk-max/BTVault")
         authors = listOf("BTcozum")
     }
 
     android {
         namespace = "com.btcozum.btvault"
+        compileSdk = 35
 
         defaultConfig {
             minSdk = 21
-            compileSdkVersion(35)
-            targetSdk = 35
         }
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
         }
 
-        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile> {
+        tasks.withType<KotlinJvmCompile>().configureEach {
             compilerOptions {
-                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8)
+                jvmTarget.set(JvmTarget.JVM_17)
+
                 freeCompilerArgs.addAll(
-                    listOf(
-                        "-Xno-call-assertions",
-                        "-Xno-param-assertions",
-                        "-Xno-receiver-assertions"
-                    )
+                    "-Xno-call-assertions",
+                    "-Xno-param-assertions",
+                    "-Xno-receiver-assertions"
                 )
             }
         }
     }
 
     dependencies {
-        val cloudstream by configurations
-        val implementation by configurations
+        add("cloudstream", "com.lagradost:cloudstream3:pre-release")
 
-        cloudstream("com.lagradost:cloudstream3:pre-release")
+        // Kotlin & Coroutines
+        add("implementation", kotlin("stdlib"))
+        add("implementation", "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.11.0")
+        add("implementation", "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
+        add("implementation", "org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
 
-        implementation(kotlin("stdlib"))
-        implementation("com.github.Blatzar:NiceHttp:0.4.13")
-        implementation("org.jsoup:jsoup:1.19.1")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
-        implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
-        implementation("com.github.vidstige:jadb:v1.2.1")
+        // Network & Scraping
+        add("implementation", "com.github.Blatzar:NiceHttp:0.4.18")
+        add("implementation", "com.squareup.okhttp3:okhttp:5.4.0")
+        add("implementation", "org.jsoup:jsoup:1.22.2")
+
+        // JSON Parsing
+        add("implementation", "com.fasterxml.jackson.module:jackson-module-kotlin:2.22.1")
+        add("implementation", "com.fasterxml.jackson.core:jackson-databind:2.22.1")
+        add("implementation", "com.google.code.gson:gson:2.14.0")
+
+        // JavaScript Evaluation
+        add("implementation", "com.faendir.rhino:rhino-android:1.6.0")
+        add("implementation", "app.cash.quickjs:quickjs-android:0.9.2")
+
+        // Utils & Core
+        add("implementation", "me.xdrop:fuzzywuzzy:1.4.0")
+        add("implementation", "androidx.core:core-ktx:1.19.0")
     }
 }
 
-task<Delete>("clean") {
+tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
